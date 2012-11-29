c7ccdef3aa9d2cef3b419b7c0e944bf8e6b508de
Eduardo Hertz <eduardohertz@gmail.com>
2012-11-29 14:36:26 -0200

    Refactoring.

--- spec/features/balance_spec.rb
+++ spec/features/balance_spec.rb
@@ -1,12 +1,14 @@
 require 'spec_helper'
 
 feature 'Send balance file' do
-	it 'should receive a file' do
-		visit root_path
-		click_link 'Enviar arquivo'
-		attach_file 'Arquivo', "#{Rails.root}/spec/data/data.tab"
-		click_button 'Enviar'
-		page.should have_content 'Arquivo enviado com sucesso.'
-		current_path.should == balances_path
+	context 'when receive a valid file' do
+		it 'redirects to balances_path' do
+			visit root_path
+			click_link 'Enviar arquivo'
+			attach_file 'Arquivo', "#{Rails.root}/spec/data/data.tab"
+			click_button 'Enviar'
+			page.should have_content 'Arquivo enviado com sucesso.'
+			current_path.should == balances_path
+		end
 	end
 end
