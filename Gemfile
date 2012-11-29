bd21b2cb63cf708d560cbce03013c1d7608926e1
Eduardo Hertz <eduardohertz@gmail.com>
2012-11-29 12:13:20 -0200

    Initialized project.

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.9'
+gem 'mysql2'
+gem 'jquery-rails'
+gem 'simple_form'
+gem 'thin'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'therubyracer', :platforms => :ruby
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :test do
+	gem 'rspec-rails'
+	gem 'capybara'
+	gem 'factory_girl_rails'
+	gem 'spork'
+	gem 'launchy'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myfreecomm::Application.config.secret_token = 'fb38477fe671a47b2d3b33306fbc46a287066db2c600a85973b93fb7a5ec8fd24d16bdf651c7d7a2ed2e4bdbb7975d11e49e81d502791b1ec5c9ad86003b9d19'
