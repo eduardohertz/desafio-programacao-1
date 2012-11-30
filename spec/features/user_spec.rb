e0a14042e052dd48a810a07c49e05a753b4bd848
Eduardo Hertz <eduardohertz@gmail.com>
2012-11-30 02:15:17 -0200

    Fixed some details.

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,4 +1,16 @@
+# -*- encoding : utf-8 -*-
 require 'spec_helper'
+
+feature 'User rules' do
+	context 'when not logged in' do
+		it 'do not show links in menu' do
+			visit root_path
+			page.should_not have_content 'Enviar arquivo'
+			page.should_not have_content 'Receita bruta total'
+			page.should_not have_content 'Sair'
+		end
+	end
+end
 
 feature 'Login' do
 	background do
@@ -12,5 +24,17 @@ feature 'Login' do
 		click_button 'Entrar'
 		page.should have_content 'Autenticado com sucesso.'
 		current_path.should == new_entry_path
+	end
+end
+
+feature 'Logout' do
+	background do
+		@user = FactoryGirl.create :user
+		login(@user.email, '123456')
+	end
+
+	it 'through link in menu' do
+		click_link 'Sair'
+		page.should have_content 'Você precisa registrar-se ou fazer login para continuar.'
 	end
 end
